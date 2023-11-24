9a0f31df37a05c2e86c785096797e933da14ce14
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-24 19:50:05 +0000

    Add custom widget placeholder

---
+++ lib/widgets/custom_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/flutter.dart';
+
+class CustomWidget extends Statelesswidget {
+  CustomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Custom Widget'));
+  }
+}
