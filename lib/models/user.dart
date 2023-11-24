e78bb43446241adc67bb9bf9bfd050999ac98a82
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-24 19:48:28 +0000

    Add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/flutter.dart';
+
+class User {
+  final String userId;
+  final String name;
+  final String email;
+
+  User({
+    required this.userId,
+    required this.name,
+    required this.email,
+  });
+}
