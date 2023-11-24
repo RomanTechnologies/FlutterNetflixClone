c3d3d18c76939589a6a3d2fcac397772fe15ee6b
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-24 19:55:06 +0000

    Implement API service for fetching movies

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,22 @@
+import 'dart:async';
 import 'package:flutter/flutter.dart';
+import 'package:http/http.dart' as http;
+
+import 'models/movie.dart';
 
 class ApiService {
-  // Placeholder for API communication logic.
+  final string baseUrl = 'https://example.com/api';  // Replace with real API URL
+  
+void GetMoviesAsync() async {
+    try {
+      var response = await http.get(Uri.parse('$baseUrl/movies'));
+      if (response.statusCode == 200) {
+        // Parse response and return movies list
+      } else {
+        throw 'Failed to load movies';
+      }
+    } catch (e) {
+      throw 'error';
+    }
+  }
 }
